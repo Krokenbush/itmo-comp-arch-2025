bd2959a7d6d414e60fb7678d0c0edc093384397d
Krokenbush <113181041+Krokenbush@users.noreply.github.com>
2025-12-26 22:27:38 +0300

    Update riscv_cpu.v

--- hw2/riscv_cpu.v
+++ hw2/riscv_cpu.v
@@ -5,25 +5,18 @@ module riscv_cpu(clk, pc, pc_new, instruction_memory_a, instruction_memory_rd, d
   // текущее значение регистра PC
   inout [31:0] pc;
   // новое значение регистра PC (адрес следующей команды)
-  output [31:0] pc_new;
   output reg [31:0] pc_new;
   // we для памяти данных
-  output data_memory_we;
   output reg data_memory_we;
   // адреса памяти и данные для записи памяти данных
-  output [31:0] instruction_memory_a, data_memory_a, data_memory_wd;
   output reg  [31:0] instruction_memory_a, data_memory_a, data_memory_wd;
   // данные, полученные в результате чтения из памяти
-  inout [31:0] instruction_memory_rd, data_memory_rd;
   inout reg  [31:0] instruction_memory_rd, data_memory_rd;
   // we3 для регистрового файла
-  output register_we3;
   output reg  register_we3;
   // номера регистров
-  output [4:0] register_a1, register_a2, register_a3;
   output reg  [4:0] register_a1, register_a2, register_a3;
   // данные для записи в регистровый файл
-  output [31:0] register_wd3;
   output reg  [31:0] register_wd3;
   // данные, полученные в результате чтения из регистрового файла
   inout [31:0] register_rd1, register_rd2;
